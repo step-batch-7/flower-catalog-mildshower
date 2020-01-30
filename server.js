ada4dd765ac7f1b025bcb0a21dff97e513f84531
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-01-31 01:01:57 +0530

    added main function to server.js
    previously everything was in module level scope, now created main
    function

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const {createServer} = require('http');
 const {info} = require('console');
 const {app} = require('./handlers.js');
 
-const server = createServer();
+const main = function(){
+  const server = createServer();
+  server.on('request', (req, res) => app.serve(req, res));
+  server.on('close', () => info('server is closed'));
+  const [,, port] = process.argv;
+  server.listen(port, () => {
+    info('server is listening on :', server.address().port);
+  });
+};
 
-server.on('request', (req, res) => app.serve(req, res));
-server.on('close', () => {
-  info('server is closed');
-});
-
-server.listen(process.argv[2], () => {
-  info('server is listening on :', server.address().port);
-});
+main();
